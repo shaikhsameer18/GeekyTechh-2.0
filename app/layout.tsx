c03edf5c773660bfaa6b09a81453f64906247389
Sameer Ahmed <sameer.shaikh0425@gmail.com>
2024-09-09 18:52:22 +0530

    Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,33 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
-import { ThemeProvider } from 'next-themes'
-
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+import { ThemeProvider } from 'next-themes';
 
 export const metadata: Metadata = {
   title: "GeekyTechh",
   description: "Freelance Service Providing Company",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
+        <Script
+          id="google-tag-manager"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KQSR8W79');
+            `,
+          }}
+        />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
